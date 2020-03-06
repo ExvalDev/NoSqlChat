d6f7afa9aa016e0c8c4c17cdf06fc0bdc469591e
chriskch <chriskoch170@gmail.com>
2020-03-06 15:58:31 +0100

    Lifetime

--- client/src/app/main-page/_components/post/post.component.ts
+++ client/src/app/main-page/_components/post/post.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Post } from "../../_interfaces/main";
 import { MainSocketService } from "../../main-socket.service";
+import { CompileShallowModuleMetadata } from '@angular/compiler';
 
 @Component({
   selector: 'app-post',
@@ -19,11 +20,57 @@ export class PostComponent implements OnInit {
   }
 
   like(post:string){
-    this.mainSocket.like(post);
+    if(localStorage.getItem("userLikes") != undefined){
+      var userLikes = JSON.parse(localStorage.getItem('userLikes'));
+      this.mainSocket.like(post);
+      if(!userLikes.includes(post))
+      {
+        var likeCount = Number(document.getElementById('likeCount'+post).innerText);
+        likeCount += 1;
+        document.getElementById('likeCount'+post).innerHTML = String(likeCount);
+        document.getElementById('likeArea'+post).style.color = '#fdc13f';
+        userLikes.push(post);
+        localStorage.setItem('userLikes', JSON.stringify(userLikes));
+        console.log(JSON.parse(localStorage.getItem('userLikes')));
+      }
+    }
+    else{
+      this.mainSocket.like(post);
+      var likeCount = Number(document.getElementById('likeCount'+post).innerText);
+      likeCount += 1;
+      document.getElementById('likeCount'+post).innerHTML = String(likeCount);
+      document.getElementById('likeArea'+post).style.color = '#fdc13f';
+      var userLikesArray: string[] = [];
+      userLikesArray.push(post);
+      localStorage.setItem('userLikes', JSON.stringify(userLikesArray));
+      console.log(JSON.parse(localStorage.getItem('userLikes')));
+    }
   }
 
   dislike(post:string){
-    this.mainSocket.dislike(post);
+    if(localStorage.getItem("userDislikes") != undefined){
+      var userDislikes = JSON.parse(localStorage.getItem('userDislikes'));
+      this.mainSocket.dislike(post);
+      if(!userDislikes.includes(post))
+      {
+        var dislikeCount = Number(document.getElementById('dislikeCount'+post).innerText);
+        dislikeCount += 1;
+        document.getElementById('dislikeCount'+post).innerHTML = String(dislikeCount);
+        document.getElementById('dislikeArea'+post).style.color = '#a57555';
+        userDislikes.push(post);
+        localStorage.setItem('userDislikes', JSON.stringify(userDislikes));
+      }
+    }
+    else{
+      this.mainSocket.like(post);
+      var dislikeCount = Number(document.getElementById('dislikeCount'+post).innerText);
+      dislikeCount += 1;
+      document.getElementById('dislikeCount'+post).innerHTML = String(dislikeCount);
+      document.getElementById('dislikeArea'+post).style.color = '#a57555';
+      var userDislikesArray: string[] = [];
+      userDislikesArray.push(post);
+      localStorage.setItem('userDislikes', JSON.stringify(userDislikesArray));
+    }
   }
 
   follow(followUser:string){
